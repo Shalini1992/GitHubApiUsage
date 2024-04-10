cef47dafc3f7567fcd6adaac40949d6f4d534775
shalini.m <shalini.m@laminaar.in>
2024-04-10 12:19:40 +0530

    token is changed

--- GitHubApiUsage/Utility/Constants.swift
+++ GitHubApiUsage/Utility/Constants.swift
@@ -7,6 +7,6 @@
 
 import Foundation
 
-let GitHubAccessToken = "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"
+let GitHubAccessToken = "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"
 
 let issuesPerPageCount  = 15
